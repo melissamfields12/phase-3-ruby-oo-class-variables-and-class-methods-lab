5b9e6d312f5e877a020009dd898b18364bcebb30
Melissa Fields <melissamfields12@gmail.com>
2023-03-29 15:17:12 -0700

    commit tree 1a331e59333cf901e704530852e718fbbb206461

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    @@genre_count = []
+    @@artist_count = []
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+
+        @@count += 1
+        @@artists << @artist
+        @@genres << @genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artist_count
+        @@artists.tally
+    end
+
+end
